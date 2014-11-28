024270a211abaa411af556742d9a5e99fa05c880
jeanneo22 <jeanneop22@gmail.com>
2014-11-27 22:41:12 -0200

    Arquivos e classes do projeto

---
+++ src/Personagens/Herois/Superman.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Personagens.Herois;
+
+/**
+ *
+ * @author Jeanne
+ */
+public class Superman extends Heroi{
+
+    @Override
+    public void atacar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void defender() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mover() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
